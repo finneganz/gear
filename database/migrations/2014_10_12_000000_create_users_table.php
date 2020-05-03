de0415c13e98f2262ca200cd6ab0d7a9e0b8148c
finneganz <abs0lutely.n0nsense.cs@gmail.com>
2020-05-03 17:11:23 +0900

    Chore mics-table migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->undignedBigInteger('mouse_id')->foreign('mouse_id')->references('id')->on('mouses')->onDelete('set null')->nullable();
+            $table->undignedBigInteger('mousepad_id')->foreign('mousepad_id')->references('id')->on('mousepads')->onDelete('set null')->nullable();
+            $table->undignedBigInteger('mousebungee_id')->foreign('mousebungee_id')->references('id')->on('mousebungees')->onDelete('set null')->nullable();
+            $table->undignedBigInteger('keyboard_id')->foreign('keyboard_id')->references('id')->on('keyboards')->onDelete('set null')->nullable();
+            $table->undignedBigInteger('headset_id')->foreign('headset_id')->references('id')->on('headsets')->onDelete('set null')->nullable();
+            $table->undignedBigInteger('mic_id')->foreign('mic_id')->references('id')->on('mics')->onDelete('set null')->nullable();
+            $table->undignedBigInteger('monitor_id')->foreign('monitor_id')->references('id')->on('monitors')->onDelete('set null')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2020_05_03_074906_create_mics_table.php
+++ database/migrations/2020_05_03_074906_create_mics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminsTable extends Migration
+class CreateMicsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('mics', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->undignedBigInteger('user_id')->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('mics');
     }
 }

2fd5cbea2f02bb77c259a1c6253dcaeec7df33bc
yasuha-h <kurukami135@gmail.com>
2020-05-03 19:29:09 +0900

    Chore initial at all device models

---
+++ app/Models/Devices/Keyboard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Devices;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Keyboard extends Model
+{
+    //
+}
---
+++ app/Models/Devices/Mousebungee.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Devices;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mousebungee extends Model
+{
+    //
+}

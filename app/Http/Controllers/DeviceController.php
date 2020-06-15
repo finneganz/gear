78b08832c3d78b97ef6d097779116ed1f4c5ed06
yasuha-h <kurukami135@gmail.com>
2020-06-15 23:11:34 +0900

    Add get devices product

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -2,7 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Devices\Maker;
+use App\Models\Devices\Headset;
+use App\Models\Devices\Keyboard;
+use App\Models\Devices\Mic;
+use App\Models\Devices\Monitor;
+use App\Models\Devices\Mouse;
+use App\Models\Devices\Mousebungee;
+use App\Models\Devices\Mousepad;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
 
 class DeviceController extends Controller
 {
@@ -10,8 +19,43 @@ class DeviceController extends Controller
     {
         return view('devices.list');
     }
-    public function showDeviceProduct()
+    public function showDeviceProduct(Router $router)
     {
+        $routeParams = $router->getCurrentRoute()->parameters();
+        $deviceParam = $routeParams['device'];
+        $makerParam = $routeParams['maker'];
+        $productParam = $routeParams['product'];
+
+        $maker_name = Maker::where('maker_name', $makerParam)->first()->maker_name;
+        switch ($deviceParam) {
+            case 'headsets':
+                $headset = Headset::where('headset_name', $productParam)->first();
+                break;
+            case 'keyboards':
+                $keyboard = Keyboard::where('keyboard_name', $productParam)->first();
+                break;
+            case 'mics':
+                $mic = Mic::where('mic_name', $productParam)->first();
+                break;
+            case 'monitors':
+                $monitor = Monitor::where('monitor_name', $productParam)->first();
+                break;
+            case 'mouses':
+                $mouse = Mouse::where('mouse_name', $productParam)->first();
+                break;
+            case 'mousebungees':
+                $mousebungee = Mousebungee::where('mousebungee_name', $productParam)->first();
+                break;
+            case 'mousepads':
+                $mousepad = Mousepad::where('mousepad_name', $productParam)->first();
+                break;
+            default:
+                //
+                break;
+        }
+        
+
+
         return view('devices.product');
     }
 

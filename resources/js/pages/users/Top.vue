c4df21059e3c9c0b5935bb58e5f6c48116c3ab79
yasuha-h <kurukami135@gmail.com>
2020-07-15 22:25:16 +0900

    Add rough top page view

--- resources/js/pages/users/Top.vue
+++ resources/js/pages/users/Top.vue
@@ -1,21 +1,40 @@
 <template>
   <v-app>
     <header-component></header-component>
-    <v-content>
-      <div class="container">
-          <div class="row justify-content-center">
-              <div class="col-md-8">
-                  <div class="card">
-                      <div class="card-header">Top Component</div>
-
-                      <div class="card-body">
-                          I'm a top page component.
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </v-content>
+    <v-container fluid class="px-0">
+      <v-card flat tile width="auto" color="blue-grey lighten-4" class="py-12 mt-11">
+        <v-card-title class="justify-center">GearArchive</v-card-title>
+        <v-card-subtitle class="text-center">gamer's device sharing for gamers by gamers</v-card-subtitle>
+      </v-card>
+    </v-container>
+    <v-container>
+      <v-row justify="space-around">
+        <v-col cols="3">
+          <v-card outlined>
+            <v-card-text>img</v-card-text>
+            <v-card-title>PlayerName</v-card-title>
+            <v-card-subtitle>@twitter_id</v-card-subtitle>
+            <v-card-text>^^^^^^^^^</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="3">
+          <v-card outlined>
+            <v-card-text>img</v-card-text>
+            <v-card-title>PlayerName</v-card-title>
+            <v-card-subtitle>@twitter_id</v-card-subtitle>
+            <v-card-text>^^^^^^^^^</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="3">
+          <v-card outlined>
+            <v-card-text>img</v-card-text>
+            <v-card-title>PlayerName</v-card-title>
+            <v-card-subtitle>@twitter_id</v-card-subtitle>
+            <v-card-text>^^^^^^^^^</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 

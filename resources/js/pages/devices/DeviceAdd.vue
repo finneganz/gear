923cab5ec2799568c8f94e23b86419e35101fa63
yasuha-h <kurukami135@gmail.com>
2020-07-17 22:40:04 +0900

    Add show add device page

--- resources/js/pages/devices/DeviceAdd.vue
+++ resources/js/pages/devices/DeviceAdd.vue
@@ -1,21 +1,46 @@
 <template>
   <v-app>
     <header-component></header-component>
-    <v-content>
-      <div class="container">
-          <div class="row justify-content-center">
-              <div class="col-md-8">
-                  <div class="card">
-                      <div class="card-header">Device Add Component</div>
-
-                      <div class="card-body">
-                          I'm a device add page component.
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </v-content>
+    <v-container fluid class="px-0">
+      <v-card flat tile width="auto" color="blue-grey lighten-4" class="mt-11">
+        <v-card-title class="justify-center">add gear page</v-card-title>
+      </v-card>
+    </v-container>
+    <v-container fluid>
+      <v-card flat max-width="500" class="mx-auto">
+        <v-form
+          method="POST"
+          action="/device/add"
+          id="add-device"
+        >
+          <input type="hidden" name="_token" :value="csrf" />
+          <v-select
+            class="pt-3"
+            :items="deviceTypes"
+            label="device type"
+            dense
+            id="deviceType"
+            name="deviceType"
+          ></v-select>
+          <v-text-field
+            label="device name"
+            id="deviceName"
+            name="deviceName"
+          ></v-text-field>
+          <v-text-field
+            label="maker name"
+            id="makerName"
+            name="makerName"
+          >
+          </v-text-field>
+          <v-btn
+            type="submit"
+            color="primary"
+            class="text-capitalize"
+          >submit</v-btn>
+        </v-form>
+      </v-card>
+    </v-container>
   </v-app>
 </template>
 
@@ -27,6 +52,25 @@ export default {
   },
   mounted() {
     console.log('Component mounted.')
-  }
+  },
+  data: () => ({
+    deviceTypes: [
+        'headset',
+        'keyboard',
+        'mic',
+        'monitor',
+        'mouse',
+        'mousebungee',
+        'mousepad',
+    ],
+    csrf: 
+    document.querySelector('meta[name="csrf-token"]')
+    .getAttribute('content'),
+    methods: {
+      addDevice: function(){
+        document.querySelector('add-device').submit();
+      },
+    },
+  })
 }
 </script>

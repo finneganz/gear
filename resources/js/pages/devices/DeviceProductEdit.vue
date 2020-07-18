003a0dd29f9eeb1010c1a94b76a5ad2fc86a1998
yasuha-h <kurukami135@gmail.com>
2020-07-18 17:09:57 +0900

    Add show edit device page

--- resources/js/pages/devices/DeviceProductEdit.vue
+++ resources/js/pages/devices/DeviceProductEdit.vue
@@ -1,21 +1,38 @@
 <template>
   <v-app>
     <header-component></header-component>
-    <v-content>
-      <div class="container">
-          <div class="row justify-content-center">
-              <div class="col-md-8">
-                  <div class="card">
-                      <div class="card-header">Device Product Edit Component</div>
-
-                      <div class="card-body">
-                          I'm a device product edit page component.
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </v-content>
+    <v-container fluid class="px-0">
+      <v-card flat tile width="auto" color="blue-grey lighten-4" class="mt-11">
+        <v-card-title class="justify-center">device product edit page</v-card-title>
+      </v-card>
+    </v-container>
+    <v-container fluid>
+      <v-card flat max-width="500" class="mx-auto">
+        <v-form
+          method="POST"
+          action="/devices/headsets/steelseries/siberia_v2/edit"
+          id="edit-device"
+        >
+          <input type="hidden" name="_token" :value="csrf" />
+          <v-text-field
+            label="device name"
+            id="deviceName"
+            name="deviceName"
+          ></v-text-field>
+          <v-text-field
+            label="maker name"
+            id="makerName"
+            name="makerName"
+          >
+          </v-text-field>
+          <v-btn
+            type="submit"
+            color="primary"
+            class="text-capitalize mt-3"
+          >submit</v-btn>
+        </v-form>
+      </v-card>
+    </v-container>
   </v-app>
 </template>
 
@@ -27,6 +44,16 @@ export default {
   },
   mounted() {
     console.log('Component mounted.')
-  }
+  },
+  data: () => ({
+    csrf: 
+    document.querySelector('meta[name="csrf-token"]')
+    .getAttribute('content'),
+    methods: {
+      addDevice: function(){
+        document.querySelector('edit-device').submit();
+      },
+    },
+  })
 }
 </script>

5c4936291ef9d2d97f3f2fc71164d6fd1a017b33
yasuha-h <kurukami135@gmail.com>
2020-07-24 16:50:53 +0900

    Update header style and link

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,15 +1,62 @@
 <template>
-    <v-app-bar app dark color="primary">
-      <v-col cols="1"></v-col>
-      <v-toolbar-title>GearArchive</v-toolbar-title>
-      <v-col cols="1"></v-col>
-      <v-col cols="2">ユーザ一覧</v-col>
-      <v-col cols="2">デバイス一覧</v-col>
-      <v-col cols="2">設定</v-col>
-      <v-spacer></v-spacer>
-      <v-btn cols="2" color="light-blue">login</v-btn>
-    </v-app-bar>
+  <v-app-bar app dark color="primary">
+    <v-col cols="1"></v-col>
+    <v-card 
+      flat
+      :ripple="false"
+      color="primary"
+      href="/"
+      width="150"
+      class="text-center"
+    >
+      GearArchive
+    </v-card>
+    <v-col cols="1"></v-col>
+    <v-col cols="2">
+      <v-card
+        flat
+        :ripple="false"
+        color="primary"  
+        href="/"
+        class="text-center"
+      >
+        ユーザ一覧
+      </v-card>
+    </v-col>
+    <v-col cols="2">
+      <v-card
+        flat
+        :ripple="false"
+        color="primary"  
+        href="/devices"
+        class="text-center"
+      >
+        デバイス一覧
+      </v-card>
+    </v-col>
+    <v-col cols="2">
+      <v-card
+        flat
+        :ripple="false"
+        color="primary"  
+        href="/users/1"
+        class="text-center"
+        max-width="100"
+      >
+        設定
+      </v-card>
+    </v-col>
+    <v-spacer></v-spacer>
+    <v-btn
+      cols="2"
+      color="light-blue"
+      href="/login"
+    >
+      login
+    </v-btn>
+  </v-app-bar>
 </template>
+
 <script>
     export default {
         mounted() {

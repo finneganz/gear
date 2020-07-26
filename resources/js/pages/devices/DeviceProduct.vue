5e4909c84103dac9ea2f41aefa9ecad603402989
yasuha-h <kurukami135@gmail.com>
2020-07-26 16:40:55 +0900

    Add rough view device product page

--- resources/js/pages/devices/DeviceProduct.vue
+++ resources/js/pages/devices/DeviceProduct.vue
@@ -1,21 +1,18 @@
 <template>
   <v-app>
     <header-component></header-component>
-    <v-content>
-      <div class="container">
-          <div class="row justify-content-center">
-              <div class="col-md-8">
-                  <div class="card">
-                      <div class="card-header">Device Product Component</div>
-
-                      <div class="card-body">
-                          I'm a device product page component.
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </v-content>
+    <v-container fluid class="mt-12">
+      <v-row justify="center" class="pt-8">
+        <v-col cols="1">
+          <v-card-text>img</v-card-text>
+        </v-col>
+        <v-card flat max-width="300">
+          <v-card-title>Device Name</v-card-title>
+          <v-card-text>Device's maker</v-card-text>
+          <v-btn outlined small color="primary" class="ml-4">Link</v-btn>
+        </v-card>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 

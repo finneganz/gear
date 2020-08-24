56932088318ad4bf77877dbb60a531626a358366
yasuha-h <kurukami135@gmail.com>
2020-08-24 18:13:23 +0900

    Refactor process of showDeviceProduct

--- app/Domains/DeviceDomain.php
+++ app/Domains/DeviceDomain.php
@@ -78,4 +78,53 @@ class DeviceDomain
         }
         return $devices;
     }
+    public function getProductOfDevice(Array $routeParams)
+    {
+        $deviceParam = $routeParams['device'];
+        $makerParam = $routeParams['maker'];
+        $productParam = $routeParams['product'];
+
+        $makerId = Maker::where('maker_name', $makerParam)->first()->id;
+        switch ($deviceParam) {
+            case 'headsets':
+                $device = Headset::where('device_name', $productParam)->where('maker_id', $makerId)->first();
+                $device->device_name = str_replace('_', ' ', $device->device_name);
+                $device->maker_name = Headset::find($device->id)->getMaker->maker_name;
+                break;
+            case 'keyboards':
+                $device = Keyboard::where('device_name', $productParam)->where('maker_id', $makerId)->first();
+                $device->device_name = str_replace('_', ' ', $device->device_name);
+                $device->maker_name = Keyboard::find($device->id)->getMaker->maker_name;
+                break;
+            case 'mics':
+                $device = Mic::where('device_name', $productParam)->where('maker_id', $makerId)->first();
+                $device->device_name = str_replace('_', ' ', $device->device_name);
+                $device->maker_name = Mic::find($device->id)->getMaker->maker_name;
+                break;
+            case 'monitors':
+                $device = Monitor::where('device_name', $productParam)->where('maker_id', $makerId)->first();
+                $device->device_name = str_replace('_', ' ', $device->device_name);
+                $device->maker_name = Monitor::find($device->id)->getMaker->maker_name;
+                break;
+            case 'mouses':
+                $device = Mouse::where('device_name', $productParam)->where('maker_id', $makerId)->first();
+                $device->device_name = str_replace('_', ' ', $device->device_name);
+                $device->maker_name = Mouse::find($device->id)->getMaker->maker_name;
+                break;
+            case 'mousebungees':
+                $device = Mousebungee::where('device_name', $productParam)->where('maker_id', $makerId)->first();
+                $device->device_name = str_replace('_', ' ', $device->device_name);
+                $device->maker_name = Mousebungee::find($device->id)->getMaker->maker_name;
+                break;
+            case 'mousepads':
+                $device = Mousepad::where('device_name', $productParam)->where('maker_id', $makerId)->first();
+                $device->device_name = str_replace('_', ' ', $device->device_name);
+                $device->maker_name = Mousepad::find($device->id)->getMaker->maker_name;
+                break;
+            default:
+                //
+                break;
+        }
+        return $device;
+    }
 }

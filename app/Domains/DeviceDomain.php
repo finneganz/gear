df7fd854c7a504338a955d59dc767ca3f6fabf29
yasuha-h <kurukami135@gmail.com>
2020-08-26 18:46:43 +0900

    Refactor be short code

--- app/Domains/DeviceDomain.php
+++ app/Domains/DeviceDomain.php
@@ -18,63 +18,33 @@ class DeviceDomain
         switch ($deviceGenreParam) {
             case 'headsets':
                 $devices = Headset::orderBy('id', 'ASC')->take(10)->get();
-                foreach($devices as $device)
-                {
-                    $device->device_name = str_replace('_', ' ', $device->device_name);
-                    $device->maker_name = Headset::find($device->id)->getMaker->maker_name;
-                }
                 break;
             case 'keyboards':
                 $devices = Keyboard::orderBy('id', 'ASC')->take(10)->get();
-                foreach($devices as $device)
-                {
-                    $device->device_name = str_replace('_', ' ', $device->device_name);
-                    $device->maker_name = Keyboard::find($device->id)->getMaker->maker_name;
-                }
                 break;
             case 'mics':
                 $devices = Mic::orderBy('id', 'ASC')->take(10)->get();
-                foreach($devices as $device)
-                {
-                    $device->device_name = str_replace('_', ' ', $device->device_name);
-                    $device->maker_name = Mic::find($device->id)->getMaker->maker_name;
-                }
                 break;
             case 'monitors':
                 $devices = Monitor::orderBy('id', 'ASC')->take(10)->get();
-                foreach($devices as $device)
-                {
-                    $device->device_name = str_replace('_', ' ', $device->device_name);
-                    $device->maker_name = Monitor::find($device->id)->getMaker->maker_name;
-                }
                 break;
             case 'mouses':
                 $device = Mouse::orderBy('id', 'ASC')->take(10)->get();
-                foreach($devices as $device)
-                {
-                    $device->device_name = str_replace('_', ' ', $device->device_name);
-                    $device->maker_name = Mouse::find($device->id)->getMaker->maker_name;
-                }
                 break;
             case 'mousebungees':
                 $devices = Mousebungee::orderBy('id', 'ASC')->take(10)->get();
-                foreach($devices as $device)
-                {
-                    $device->device_name = str_replace('_', ' ', $device->device_name);
-                    $device->maker_name = Mousebungee::find($device->id)->getMaker->maker_name;
-                }
                 break;
             case 'mousepads':
                 $devices = Mousepad::orderBy('id', 'ASC')->take(10)->get();
-                foreach($devices as $device)
-                {
-                    $device->device_name = str_replace('_', ' ', $device->device_name);
-                    $device->maker_name = Mousepad::find($device->id)->getMaker->maker_name;
-                }
                 break;
             default:
                 //
                 break;
+        }
+        foreach($devices as $device)
+        {
+            $device->device_name = str_replace('_', ' ', $device->device_name);
+            $device->maker_name = $device->getMaker->maker_name;
         }
         return $devices;
     }
@@ -118,56 +88,32 @@ class DeviceDomain
         switch ($request->deviceType) {
             case 'headset':
                 $device = new Headset;
-                $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
-                $device->device_name = $request->input('deviceName');
-                $device->maker_id = $makerId;
-                $device->save();
                 break;
             case 'keyboard':
                 $device = new Keyboard;
-                $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
-                $device->device_name = $request->input('deviceName');
-                $device->maker_id = $makerId;
-                $device->save();
                 break;
             case 'mic':
                 $device = new Mic;
-                $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
-                $device->device_name = $request->input('deviceName');
-                $device->maker_id = $makerId;
-                $device->save();
                 break;
             case 'monitor':
                 $device = new Monitor;
-                $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
-                $device->device_name = $request->input('deviceName');
-                $device->maker_id = $makerId;
-                $device->save();
                 break;
             case 'mouse':
                 $device = new Mouse;
-                $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
-                $device->device_name = $request->input('deviceName');
-                $device->maker_id = $makerId;
-                $device->save();
                 break;
             case 'mousebungee':
                 $device = new Mousebungee;
-                $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
-                $device->device_name = $request->input('deviceName');
-                $device->maker_id = $makerId;
-                $device->save();
                 break;
             case 'mousepad':
                 $device = new Mousepad;
-                $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
-                $device->device_name = $request->input('deviceName');
-                $device->maker_id = $makerId;
-                $device->save();
                 break;
             default:
                 //
                 break;
         }
+        $makerId = Maker::where('maker_name', $request->makerName)->first()->id;
+        $device->device_name = $request->input('deviceName');
+        $device->maker_id = $makerId;
+        $device->save();
     }
 }

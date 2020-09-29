f6d0cc8bcd99d9a63fad2516ccf80a7675583284
yasuha-h <kurukami135@gmail.com>
2020-09-29 18:51:32 +0900

    Add admin yasuha and finnegantz

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class AdminsTableSeeder extends Seeder
 {
@@ -11,6 +12,22 @@ class AdminsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('admins')->truncate();
+        $now = Carbon::now();
+
+        DB::table('admins')->insert([
+            [
+                // yasuha
+                'user_id' => 1,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                // finnegantz
+                'user_id' => 2,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+        ]);
     }
 }

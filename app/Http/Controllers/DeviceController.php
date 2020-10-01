fa96c71d927ccb8482dafc274180c022de340d9f
yasuha-h <kurukami135@gmail.com>
2020-10-01 22:20:21 +0900

    Add redirect when accessed by non-admin

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -46,36 +46,64 @@ class DeviceController extends BaseController
     // 管理者用
     public function showDeviceAddPage()
     {
-        $isLoggedIn = $this->authCheck();
-        return view('devices.add', compact('isLoggedIn'));
+        if(Auth::id() === 1 || Auth::id() === 2)
+        {
+            $isLoggedIn = $this->authCheck();
+            return view('devices.add', compact('isLoggedIn'));
+        }
+        else
+        {
+            return redirect()->action('UserController@showUserList');
+        }
     }
     public function addDevice(AddDeviceRequest $request)
     {
-        $deviceDomain = new DeviceDomain;
-        $device = $deviceDomain->addNewDevice($request);
-        $deviceDomain->save($device);
-
-        return redirect()->action('DeviceController@showDeviceList');
+        if(Auth::id() === 1 || Auth::id() === 2)
+        {
+            $deviceDomain = new DeviceDomain;
+            $device = $deviceDomain->addNewDevice($request);
+            $deviceDomain->save($device);
+    
+            return redirect()->action('DeviceController@showDeviceList');
+        }
+        else
+        {
+            return redirect()->action('UserController@showUserList');
+        }
     }
     public function showDeviceEditPage(Router $router)
     {
-        $routeParams = $router->getCurrentRoute()->parameters();
-        $deviceDomain = new DeviceDomain;
-        $device = $deviceDomain->getProductOfDevice($routeParams);
-        $device->maker_name = $device->getMaker->maker_name;
-        $isLoggedIn = $this->authCheck();
-
-        return view('devices.edit', compact('device', 'isLoggedIn'));
+        if(Auth::id() === 1 || Auth::id() === 2)
+        {
+            $routeParams = $router->getCurrentRoute()->parameters();
+            $deviceDomain = new DeviceDomain;
+            $device = $deviceDomain->getProductOfDevice($routeParams);
+            $device->maker_name = $device->getMaker->maker_name;
+            $isLoggedIn = $this->authCheck();
+    
+            return view('devices.edit', compact('device', 'isLoggedIn'));
+        }
+        else
+        {
+            return redirect()->action('UserController@showUserList');
+        }
     }
     public function editDevice(Router $router, EditDeviceRequest $request)
     {
-        $routeParams = $router->getCurrentRoute()->parameters();
-        $deviceDomain = new DeviceDomain;
-        $device = $deviceDomain->getProductOfDevice($routeParams);
-        $device->device_name = $request->input('deviceName');
-        $device->maker_id = Maker::where('maker_name', $request->input('makerName'))->first()->id;
-        $deviceDomain->save($device);
-
-        return redirect()->action('DeviceController@showDeviceList');
+        if(Auth::id() === 1 || Auth::id() === 2)
+        {
+            $routeParams = $router->getCurrentRoute()->parameters();
+            $deviceDomain = new DeviceDomain;
+            $device = $deviceDomain->getProductOfDevice($routeParams);
+            $device->device_name = $request->input('deviceName');
+            $device->maker_id = Maker::where('maker_name', $request->input('makerName'))->first()->id;
+            $deviceDomain->save($device);
+    
+            return redirect()->action('DeviceController@showDeviceList');
+        }
+        else
+        {
+            return redirect()->action('UserController@showUserList');
+        }
     }
 }

be2c06abcfeceb9a92a9f23700984771bd826c11
yasuha-h <kurukami135@gmail.com>
2020-10-08 09:37:27 +0900

    Add kind of mic

--- database/seeds/Devices/MicsTableSeeder.php
+++ database/seeds/Devices/MicsTableSeeder.php
@@ -16,6 +16,8 @@ class MicsTableSeeder extends Seeder
 
         $now = Carbon::now();
         DB::table('mics')->insert([
+            // Logicool
+                // Yeti
             [
                 'device_name' => 'Yeti',
                 'maker_id' => 2,
@@ -23,13 +25,46 @@ class MicsTableSeeder extends Seeder
                 'updated_at' => $now,
             ],
             [
+                'device_name' => 'Yeti_Nano',
+                'maker_id' => 2,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'device_name' => 'Yeti_Pro',
+                'maker_id' => 2,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'device_name' => 'Snowball_iCE',
+                'maker_id' => 2,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            // Razer
+                // Seiren
+            [
                 'device_name' => 'SEIREN_X',
                 'maker_id' => 4,
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
             [
-                'device_name' => 'Strix_Magnus',
+                'device_name' => 'SEIREN_ELITE',
+                'maker_id' => 4,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'device_name' => 'SEIREN_EMOTE',
+                'maker_id' => 4,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            // ASUS
+            [
+                'device_name' => 'ROG_Strix_Magnus',
                 'maker_id' => 5,
                 'created_at' => $now,
                 'updated_at' => $now,

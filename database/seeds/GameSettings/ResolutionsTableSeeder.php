328b3d65ec74251019e08d7acd763f32bb199451
yasuha-h <kurukami135@gmail.com>
2020-11-04 19:21:52 +0900

    Add all csgo resolutions

--- database/seeds/GameSettings/ResolutionsTableSeeder.php
+++ database/seeds/GameSettings/ResolutionsTableSeeder.php
@@ -16,6 +16,19 @@ class ResolutionsTableSeeder extends Seeder
 
         $now = Carbon::now();
         DB::table('resolutions')->insert([
+            // 4:3
+            [
+                'aspect' => 0,
+                'resolution' => '640x480',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 0,
+                'resolution' => '720x576',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
             [
                 'aspect' => 0,
                 'resolution' => '800x600',
@@ -30,25 +43,93 @@ class ResolutionsTableSeeder extends Seeder
             ],
             [
                 'aspect' => 0,
+                'resolution' => '1152x864',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 0,
                 'resolution' => '1280x960',
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
             [
-                'aspect' => 2,
+                'aspect' => 0,
+                'resolution' => '1280x1024',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            // 16:9
+            [
+                'aspect' => 1,
+                'resolution' => '1176x664',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 1,
                 'resolution' => '1280x720',
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
             [
-                'aspect' => 2,
+                'aspect' => 1,
+                'resolution' => '1360x768',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 1,
+                'resolution' => '1366x768',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 1,
                 'resolution' => '1600x960',
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
             [
-                'aspect' => 2,
+                'aspect' => 1,
+                'resolution' => '1768x992',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 1,
                 'resolution' => '1920x1080',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            // 16:10
+            [
+                'aspect' => 2,
+                'resolution' => '720x480',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 2,
+                'resolution' => '1280x768',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 2,
+                'resolution' => '1280x800',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 2,
+                'resolution' => '1600x1024',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'aspect' => 2,
+                'resolution' => '1680x1050',
                 'created_at' => $now,
                 'updated_at' => $now,
             ],

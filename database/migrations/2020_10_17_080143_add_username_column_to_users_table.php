78da1401fd9ecad067226082f37590f739e3a02d
yasuha-h <kurukami135@gmail.com>
2020-12-01 23:10:06 +0900

    Fix table name specified in dropColumn on down method

--- database/migrations/2020_10_17_080143_add_username_column_to_users_table.php
+++ database/migrations/2020_10_17_080143_add_username_column_to_users_table.php
@@ -26,7 +26,7 @@ class AddUsernameColumnToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('users');
+            $table->dropColumn('username');
         });
     }
 }

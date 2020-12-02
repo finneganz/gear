845ddd9e199fa399d9a984312cba37d976883629
yasuha-h <kurukami135@gmail.com>
2020-12-02 22:41:28 +0900

    Remove UsersTableSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersTableSeeder::class,
             ConfigsTableSeeder::class,
             MonitorSettingsTableSeeder::class,
             MouseSettingsTableSeeder::class,

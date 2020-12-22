73644daa7910b04e8bb3316e87fc42597b7aa36a
yasuha-h <kurukami135@gmail.com>
2020-12-22 19:58:33 +0900

    Update Footer style

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,46 +1,31 @@
 <template>
   <v-footer absolute dark color="primary">
     <v-row>
-      <v-col cols="2">
+      <v-col cols="3">
         <v-card flat color="primary">
-          <v-card-title class="justify-center">GearArchive</v-card-title>
+          <v-card-title class="justify-center pa-0">GearArchive</v-card-title>
         </v-card>
       </v-col>
       <v-col cols="2">
-        <v-card flat color="primary">
-          <v-card-title>Menu</v-card-title>
-          <v-card-text>^^^^^^^^</v-card-text>
-          <v-card-text class="pt-0">^^^^^^^^</v-card-text>
-          <v-card-text class="pt-0">^^^^^^^^</v-card-text>
-        </v-card>
+        <v-btn href="/about" class="text-lowercase" text>about</v-btn>
       </v-col>
       <v-col cols="2">
-        <v-card flat color="primary">
-          <v-card-title>静的ページ</v-card-title>
-          <v-card-text>About</v-card-text>
-          <v-card-text class="pt-0">Contact</v-card-text>
-          <v-card-text class="pt-0">Policy</v-card-text>
-        </v-card>
+        <v-btn href="/contact" class="text-lowercase" text>contact</v-btn>
+      </v-col>
+      <v-col cols="2">
+        <v-btn href="/policy" class="text-lowercase" text>policy</v-btn>
       </v-col>
       <v-spacer></v-spacer>
-      <v-col cols="2">
-        <v-card flat color="primary">
-          <v-card-title>
-            <v-row justify="space-around">
-              <v-icon>mdi-home</v-icon>
-              <v-icon>mdi-home</v-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-row>
-          </v-card-title>
-        </v-card>
-      </v-col>
+      <!-- <v-col cols="1">
+        <v-row justify="space-around">
+          <v-btn class="px-0" icon><v-icon>mdi-home</v-icon></v-btn>
+          <v-btn class="px-0" icon><v-icon>mdi-home</v-icon></v-btn>
+        </v-row>
+      </v-col> -->
     </v-row>
   </v-footer>
 </template>
+
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+
 </script>

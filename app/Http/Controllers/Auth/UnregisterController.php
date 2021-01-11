fde8c304066d4ad19690dd4c7f433114e376ea1d
yasuha-h <kurukami135@gmail.com>
2021-01-11 22:55:37 +0900

    Add fucntion unregister

--- app/Http/Controllers/Auth/UnregisterController.php
+++ app/Http/Controllers/Auth/UnregisterController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Users\User;
+use App\Http\Controllers\BaseController;
 
 class UnregisterController extends BaseController
 {
-    //
+    public function showUnregister()
+    {
+        $auth = $this->getAuthUser();
+        return view('auth.unregister', compact('auth'));
+    }
+    public function unregister()
+    {
+        User::where('id', Auth::id())->first()->forceDelete();
+        return redirect()->action('UserController@showUserList');
+    }
 }
